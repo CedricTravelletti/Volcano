6fe79891cf02324c6937f75f90b634b14a92eeac
Cedric Travelletti <cedrictravelletti@gmail.com>
2018-12-04 11:18:00 +0100

    Added tests for loading and grid regularizing. Tests pass.

---
+++ tests/test_loading.py
@@ -0,0 +1,48 @@
+""" Test the loading module.
+
+"""
+from volcapy import loading as ld
+from volcapy import grid as gd
+
+from numpy.testing import assert_array_equal
+
+data_path = "/home/cedric/PHD/Dev/Volcano/data/Cedric.mat"
+
+
+def test_loading():
+    data = ld.load_niklas(data_path)
+
+def test_regularize_grid():
+    unreg_grid = [
+            [0, 0, 0],
+            [1, 1, 1]]
+    dx = 1.0
+    dy = 1.0
+    dz = 0.25
+
+    # How the result should look.
+    reg_grid = [
+            [0, 0, 0],
+            [1, 0, 0],
+            [0, 1, 0],
+            [1, 1, 0],
+            [0, 0, 0.25],
+            [1, 0, 0.25],
+            [0, 1, 0.25],
+            [1, 1, 0.25],
+            [0, 0, 0.5],
+            [1, 0, 0.5],
+            [0, 1, 0.5],
+            [1, 1, 0.5],
+            [0, 0, 0.75],
+            [1, 0, 0.75],
+            [0, 1, 0.75],
+            [1, 1, 0.75],
+            [0, 0, 1],
+            [1, 0, 1],
+            [0, 1, 1],
+            [1, 1, 1]]
+
+    result = gd.regularize_grid(unreg_grid, dx, dy, dz)
+
+    assert_array_equal(result, reg_grid)
