04cccf506c9bd6628a3abbc7653cb6362828b7fa
Cedric Travelletti <cedrictravelletti@gmail.com>
2018-12-17 15:47:35 +0100

    Re-introduced get_covariance between two model cells in fast covariance.

--- volcapy/fast_covariance.pyx
+++ volcapy/fast_covariance.pyx
@@ -16,7 +16,7 @@ def build_cov(double[:, :] coords, double[:, :] out, int row_begin, int row_end)
     cdef int dim_j = coords.shape[0]
     cdef int D = coords.shape[1]
 
-    cdef double sigma_2 = 200.0
+    cdef double sigma_2 = 100.0
     cdef double lambda_2 = 200.0**2
 
     cdef double dist = 0.0
@@ -37,4 +37,27 @@ def build_cov(double[:, :] coords, double[:, :] out, int row_begin, int row_end)
                 dist += (coords[row_ind, d] - coords[j, d])**2
             out[i, j] = sigma_2 * exp(- dist / lambda_2)
 
+    return out
+
+
+def get_cov(double[:, :] coords, int i, int j):
+    """ Gets the covariance between two points in model space.
+    Can be used to build the covariance matrix.
+
+    Parameters
+    ----------
+    coords
+
+    """
+    cdef int D = coords.shape[1]
+
+    cdef double sigma_2 = 100.0
+    cdef double lambda_2 = 200.0**2
+    cdef double dist = 0.0
+    cdef float out = 0.0
+
+    for d in range(D):
+        dist += (coords[i, d] - coords[j, d])**2
+
+    out = sigma_2 * exp(- dist / lambda_2)
     return out
