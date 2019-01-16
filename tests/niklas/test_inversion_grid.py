7c3922d8a6ec9ab36a84daefa6bc17feb279f462
Cedric Travelletti <cedrictravelletti@gmail.com>
2019-01-16 15:53:46 +0100

    Implemented inversion grid with tests.

---
+++ tests/niklas/test_inversion_grid.py
@@ -0,0 +1,48 @@
+""" Test the niklas.inversion_grid module.
+
+"""
+from volcapy.niklas import dsm as dsm_mod
+from volcapy.niklas.inversion_grid import InversionGrid
+
+import numpy as np
+
+from numpy.testing import assert_array_equal
+
+
+class TestCoarsener():
+    def setUp(self):
+        longs = [1,2,3,4,5,6]
+        lats = [4,5,6,7]
+        elevations = np.array([
+                [-2,2,3,4],
+                [5, 6, 7, 8 ],
+                [10, 11, 12, 13],
+                [14, 15, 16, 17],
+                [18, 19, 20, 21],
+                [22, 23, 24, 25],
+                ])
+        dsm = dsm_mod.DSM(longs, lats, elevations)
+
+        coarsen_x = [1, 3, 2]
+        coarsen_y = [2,2]
+
+        z_levels = [-1, 0, 2, 3, 4, 5]
+        self.inversion_grid = InversionGrid(coarsen_x, coarsen_y, z_levels,
+                dsm)
+
+    def tearDown(self):
+        pass
+
+    def test_build_max_zlevels(self):
+        """ Determination of maximal zlevels on inversion grid.
+        """
+        # What we should get.
+        grid_max_zlevels = np.array([
+                [-1, 2],
+                [4, 5],
+                [5, 5],
+                ])
+
+        # Check the index correspondence.
+        assert_array_equal(self.inversion_grid.grid_max_zlevel,
+                grid_max_zlevels)
---
+++ volcapy/niklas/inversion_grid.py
@@ -0,0 +1,101 @@
+# File: inversion_grid.py, Author: Cedric Travelletti, Date: 15.01.2019.
+""" Class implementing inversion grid.
+
+The inversion grid has two importan properties
+
+* It has a coarser resolution than the dsm, meaning that a single cell in the
+ inversion grid corresponds to several cells in the dsm grid.
+ * It is irregular, i.e. it doesn't span an entire parallelepiped.
+   This is due to the fact that we don't include cells outside the volcano (in
+   the air).
+
+"""
+import numpy as np
+from volcapy.niklas.coarsener import Coarsener
+
+
+class Cell():
+    """ Class representing a cell in the inversion grid.
+    """
+    def __init__(self, x, y, z, res_x, res_y):
+        self.x = x
+        self.y =y
+        self.z = z
+        self.res_x = res_x
+        self.res_y = res_y
+
+    # Redefine printing method so it display useful informations.
+    def __str__(self):
+        return("x: {} y: {} z: {} res_x: {} res_y: {}".format(self.x, self.y,
+                self.z, self.res_x, self.res_y))
+
+
+class InversionGrid():
+    def __init__(self, coarsen_x, coarsen_y, zlevels, dsm):
+        """
+        Parameters
+        ----------
+        coarsen_x: List[int]
+            Defines the coarsening along the x-dimension.
+            For example, if coarsen_x = [10, 5, 5, ...], then the first cells
+            (along dim-x) of the coarser grid will correspond to the first 10 cells
+            of the dsm, then, the second cells (along dim-x) will correspond to the
+            next 5 cells, and so on.
+        coarsen_y: List[int]
+        zlevels: List[float]
+            List of heights (in meters) at wich we place cells.
+        dsm: DSM
+
+        """
+        self.coarsener = Coarsener(coarsen_x, coarsen_y, dsm)
+        self.dimx = self.coarsener.dimx
+        self.dimy = self.coarsener.dimy
+        self.zlevels = zlevels
+
+        self.grid_max_zlevel = self.build_max_zlevel(self.coarsener, zlevels)
+
+        # Create the grid.
+        self.fill_grid()
+
+    @staticmethod
+    def build_max_zlevel(coarsener, zlevels):
+        # Loop over coarse grid.
+        # Here we just loop over the coarsening list (each element in there
+        # corresponds to one cell in the coars grid) and use the index in the
+        # list as an index in the coarse grid.
+        grid_max_zlevel = np.zeros((coarsener.dimx, coarsener.dimy))
+
+        for i, x in enumerate(coarsener.coarsen_x):
+            for j,y in enumerate(coarsener.coarsen_y):
+                # Get the smallest elevation.
+                min_elevation = min(
+                        coarsener.get_fine_elevations(i, j))
+
+                # List of levels that are below the min elevation.
+                levels_below = [v for v in zlevels if v < min_elevation]
+
+                # If empty, then say we have at least one cell, at the minimal
+                # level.
+                if len(levels_below) == 0:
+                    grid_max_zlevel[i, j] = min(zlevels)
+
+                # Otherwise take the biggest one.
+                else:
+                    grid_max_zlevel[i, j] = max(levels_below)
+
+        return grid_max_zlevel
+
+    def fill_grid(self):
+        self.cells = []
+        for i, res_x in enumerate(self.coarsener.coarsen_x):
+            for j,res_y in enumerate(self.coarsener.coarsen_y):
+                # Get the levels (number of floors) for that cell.
+                current_max_zlevel = self.grid_max_zlevel[i, j]
+                current_zlevels = [v for v in self.zlevels if v <=
+                        current_max_zlevel]
+
+                # Loop over the floors, create the cells and append to list.
+                for z in current_zlevels:
+                    x, y = self.coarsener.get_coords(i, j)
+                    cell = Cell(x, y, z, res_x, res_y)
+                    self.cells.append(cell)
