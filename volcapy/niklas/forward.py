17849d46d73f8f1b0ffe1b0a43424a5b087b7784
Cedric Travelletti <cedrictravelletti@gmail.com>
2019-01-22 17:28:32 +0100

    Added special calculation for top cells in the forward.

--- volcapy/niklas/forward.py
+++ volcapy/niklas/forward.py
@@ -34,7 +34,8 @@ def forward(inversion_grid, data_points):
 
     return F
 
-def compute_cell_response_at_point(cell, point):
+def compute_cell_response_at_point(cell, point,
+        is_topcell=False, z_base=0):
     """ Compute the repsonse of an individual inversion cell on a measurement
     point.
 
@@ -44,6 +45,13 @@ def compute_cell_response_at_point(cell, point):
         Inversion cell whose response we want to compute.
     point: (float, float, float)
         Coordinates (x, y, z) of the point at which we measure the response.
+    is_topcell: bool
+        Should set to true when computing the repsonse of a top cell.
+        Such cells are treated differently, in that we take the prism to be
+        from the elevation, down to the base.
+        If true, then the z_base argument should be provided.
+    z_base: float
+        Altitude (in meters) of the lowest level we consider.
 
     """
 
@@ -60,13 +68,19 @@ def compute_cell_response_at_point(cell, point):
     zl = cell.z
     zh = zl + cell.res_z
 
+    # Special treatment for top cells: we go from their altitude down to the
+    # base.
+    if is_topcell:
+        zl = z_base
+        zh = cell.z
+
     return banerjee(xh, xl, yh, yl, zh, zl,
             point[0], point[1], point[2])
 
-def refine_forward(F, inversion_grid, data_points):
-    """ Refine an already computed forward. That is, modify the response of the
-    topmost cells to take the topography into account.
-
+# TODO: Currently modifies the operator in place.
+# Might be good to make it side-effect free.
+def correct_forward(F, inversion_grid, data_points, z_base):
+    """ Correct the forward at the topmost cells
     Parameters
     ----------
     F: array-like
@@ -75,19 +89,22 @@ def refine_forward(F, inversion_grid, data_points):
     data_points: List[(float, float, float)]
 
     """
-    n_cells = len(inversion_grid)
-    n_data = len(data_points)
 
     # Only loop over cells that are on the surface (i.e. 'topmost' ones).
     for i in inversion_grid.topmost_indices:
         print(i)
-        cell = inversion_grid.cells[i]
 
         # Get the fine cells that describe the topography at the current
         # (coarse) cell. Compute each response and add up.
-
+        fine_cells = inversion_grid.fine_cells_from_topmost_ind(i)
 
         for j, point in enumerate(data_points):
-            F[i, j] = compute_cell_response_at_point(cell, point)
+            # Add the responses from each fine cell.
+            temp_F = 0
+            for fine_cell in fine_cells:
+                temp_f += compute_cell_response_at_point(fine_cell, point,
+                    True, z_base)
 
-    return F
+        F[i, j] = temp_F
+
+        return F
