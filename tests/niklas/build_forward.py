6def5996f5e375600a1d64aa546ce5db3206268c
Cedric Travelletti <cedrictravelletti@gmail.com>
2019-01-22 09:00:08 +0100

    Implemented tests for the forward. Tests pass. Topmost cells not included in the tests.

---
+++ tests/niklas/build_forward.py
@@ -0,0 +1,45 @@
+""" Build the forward operator associated to Niklas's data.
+This is used for testing, in order to compare what we produce with what Niklas
+has.
+"""
+from volcapy import loading
+from volcapy.niklas.dsm import DSM
+from volcapy.niklas.inversion_grid import InversionGrid
+import volcapy.niklas.forward as fwd
+
+import numpy as np
+
+# LOAD DATA
+data_path = "/home/cedric/PHD/Dev/Volcano/data/Cedric.mat"
+dsm = DSM.from_matfile(data_path)
+
+coarsen_x = [50] + 194*[5] + [70]
+coarsen_y = [50] + 190*[5] + [75]
+
+res_x = [5000] + 194*[50] + [7000]
+res_y = [5000] + 190*[50] + [7500]
+
+z_base = -5000
+z_low = -475
+z_high = 925
+z_step = 50
+
+# Create a floor every *step*. We have to add 1 so that the z_high is included
+# in the interval.
+# TODO: May be bug prone: we have to cast to list, since arange produces an
+# array.
+z_levels = list(np.arange(z_low, z_high + 1, z_step))
+
+# Add the lowest level.
+z_levels = [z_base] + z_levels
+
+# Create the inversion grid.
+inversion_grid = InversionGrid(
+        coarsen_x, coarsen_y, res_x, res_y, z_levels, dsm)
+
+# Load the data points coordinates.
+data = loading.load_niklas(data_path)
+data_coords = data['data_coords']
+
+# Build the Forward.
+F = fwd.forward(inversion_grid, data_coords)
---
+++ tests/niklas/test_forward.py
@@ -0,0 +1,39 @@
+""" Test the computation of the forward operator. In fact, check that we get
+the same values as Niklas.
+
+"""
+# This script computes the Forward corresponding to Niklas's data.
+import build_forward as bu
+
+from nose.tools import assert_almost_equal
+
+import h5py
+import numpy as np
+
+
+data_path = "/home/cedric/PHD/Dev/Volcano/tests/niklas/F_Niklas_raw.mat"
+
+
+class TestForward():
+    def setUp(self):
+        dataset = h5py.File(data_path, 'r')
+
+        N_OBS = 543
+        N_MODEL = 179171
+        self.F_niklas_raw = np.reshape(
+                dataset['F/data'], (N_OBS, N_MODEL), order = 'F')
+
+    def test_forward(self):
+        """ Check forward agrees with Niklas.
+        """
+        # Here, compare with outputs from Niklas's code.
+        F = bu.F
+    
+        a = F[20000, 40]
+        assert_almost_equal(a, 0.001149142185113)
+    
+        a = F[100000, 500]
+        assert_almost_equal(a, 0.01927512613701765)
+    
+        a = F[80000, 10]
+        assert_almost_equal(a, 0.023827290251233535)
