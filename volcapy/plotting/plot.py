393deab3b3ae5d84eefc73d950d852f27f3341cd
Cedric Travelletti <cedrictravelletti@gmail.com>
2019-02-05 11:07:31 +0100

    Made plot compatible with lists.

--- volcapy/plotting/plot.py
+++ volcapy/plotting/plot.py
@@ -49,36 +49,6 @@ def plot(vals, x_coords, y_coords, z_coords, n_sample=0):
     plt.plot(fig)
 
 
-def plot_z_slice(slice_height, vals, x_coords, y_coords, z_coords, n_sample=0):
-    """ Same as above, but only plot as slice of fixed z coordinate.
-
-    Parameters
-    ----------
-    slice_height: float
-        Value of the z coordinate along which to slice.
-    vals: List[float]
-        List of values to plot.
-    x_coords: List[float]
-        x-coordinate of the data points. Should have the same lenght as the
-        list of values.
-    y_coords: List[float]
-    z_coords: List[float]
-    n_sample: int
-        If non zero, then will only plot n_sample randomly selected points from
-        the dataset. Useful for visualizing heavy datasets.
-
-    """
-    # Get the indices of the cells in the slice.
-    slice_inds = z_coords[:] == slice_height
-
-    # Subset the corrresponding values.
-    slice_vals = vals[slice_inds]
-    slice_x_coords = x_coords[slice_inds]
-    slice_y_coords = y_coords[slice_inds]
-    slice_z_coords = z_coords[slice_inds]
-
-    plot(slice_vals, slice_x_coords, slice_y_coords, slice_z_coords, n_sample=0)
-
 def plot_region(region_inds, vals, x_coords, y_coords, z_coords, n_sample=0):
     """ Same as above, but only plot a certain region. The region is defined by
     passing the indices of the cell in the region.
@@ -106,3 +76,38 @@ def plot_region(region_inds, vals, x_coords, y_coords, z_coords, n_sample=0):
     slice_z_coords = z_coords[region_inds]
 
     plot(slice_vals, slice_x_coords, slice_y_coords, slice_z_coords, n_sample=0)
+
+
+def plot_z_slice(slice_height, vals, x_coords, y_coords, z_coords, n_sample=0):
+    """ Same as above, but only plot as slice of fixed z coordinate.
+
+    Parameters
+    ----------
+    slice_height: float or List[float]
+        Value of the z coordinate along which to slice.
+        If a list, then will plot several slices.
+    vals: List[float]
+        List of values to plot.
+    x_coords: List[float]
+        x-coordinate of the data points. Should have the same lenght as the
+        list of values.
+    y_coords: List[float]
+    z_coords: List[float]
+    n_sample: int
+        If non zero, then will only plot n_sample randomly selected points from
+        the dataset. Useful for visualizing heavy datasets.
+
+    """
+    # If list, then have to slice several times.
+    if not isinstance(slice_height, list):
+        # Get the indices of the cells in the slice.
+        slice_inds = z_coords[:] == slice_height
+    else:
+        # Create empyt boolean array, one hot encoding of cells we will plot.
+        slice_inds = np.empty(x_coords.shape, dtype='bool')
+        slice_inds[:] = False
+
+        for h in slice_height:
+            slice_inds = np.logical_or(slice_inds, z_coords[:] == h)
+
+    plot_region(slice_inds, vals, x_coords, y_coords, z_coords, n_sample=0)
