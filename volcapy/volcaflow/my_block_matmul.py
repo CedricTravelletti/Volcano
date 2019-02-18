ee1f14e597d5c162fbc8e9cee9c42a0dd9f42f08
Ubuntu <ubuntu@ip-172-31-32-186.eu-west-1.compute.internal>
2019-02-18 16:17:08 +0000

    Pruned the graph to make it lighter.

--- volcapy/volcaflow/my_block_matmul.py
+++ volcapy/volcaflow/my_block_matmul.py
@@ -12,34 +12,54 @@ import numpy as np
 import tensorflow as tf
 
 
-niklas_data_path = "/home/cedric/PHD/Dev/Volcano/data/Cedric.mat"
+niklas_data_path = "/home/ubuntu/Volcano/data/Cedric.mat"
 
 # Load data from Niklas (forward and measurements).
 niklas_data = load_niklas(niklas_data_path)
-F = niklas_data['F']
+GT = niklas_data['F']
+GT = GT.T
+GT = tf.convert_to_tensor(GT, np.float32)
+
 coords = niklas_data['coords']
+coords = coords.astype(np.float32, copy=False)
+
 data_values = niklas_data['d']
 data_coords = niklas_data['data_coords']
 
 # TODO: This should be a placeholder, so that does not get directly loaded into
 # graph everytime.
-xs = coords[:, 0][:, None]
-F = tf.convert_to_tensor(F, np.float32)
+# xs = coords[:, 0][:, None]
 
-chunk_size = 2000
+chunk_size = 7500
 
 # ---------------------------------------------------------------------
 # ------ Build the cell distance differences for the covariance matrix.
 # ---------------------------------------------------------------------
 # Build the couples of all matchings of x with itself.
-diff_xt, diff_x = tf.meshgrid(coords[:chunk_size, 0], coords[:, 0])
-diff_yt, diff_y = tf.meshgrid(coords[:chunk_size, 1], coords[:, 1])
-diff_zt, diff_z = tf.meshgrid(coords[:chunk_size, 2], coords[:, 2])
+diff_xt, diff_x = np.meshgrid(coords[:chunk_size, 0], coords[:, 0], indexing='ij')
 
-diff = tf.stack(
-        [diff_x - diff_xt, diff_y - diff_yt, diff_z - diff_zt],
+diff_yt, diff_y = np.meshgrid(coords[:chunk_size, 1], coords[:, 1], indexing='ij')
+diff_zt, diff_z = np.meshgrid(coords[:chunk_size, 2], coords[:, 2], indexing='ij')
+
+del(coords)
+
+diffx = diff_x - diff_xt
+diffy = diff_y - diff_yt
+diffz = diff_z - diff_zt
+
+del(diff_x)
+del(diff_xt)
+del(diff_y)
+del(diff_yt)
+del(diff_z)
+del(diff_zt)
+
+diff = np.stack(
+        [diffx, diffy, diffz],
         axis=2)
-a = tf.norm(diff, axis=2)
+# a = np.linalg.norm(diff, axis=2)
+a = np.einsum('ijk,ijk->ij', diff, diff)
+del(diff)
 # ---------------------------------------------------------------------
 
 # Parameters to optimize.
@@ -66,15 +86,16 @@ for i in range(n_chunks):
 # ---------------------------------------------------------------------
 pushforward_cov = tf.stack(partial_mat_list, axis=0)
 """
-GT = tf.transpose(F)
 
-b = tf.exp(-tf.pow(a, 2))
-out = tf.matmul(tf.transpose(b), GT)
+place = tf.placeholder(tf.float32, shape=(chunk_size, GT.shape[0]))
+
+b = tf.exp(-place)
+out = tf.matmul(b, GT)
 
 from timeit import default_timer as timer
 
 with tf.Session() as sess:
     start = timer()
-    print(sess.run(out))
+    print(sess.run(out, feed_dict={place: a}))
     end = timer()
     print(end - start)
