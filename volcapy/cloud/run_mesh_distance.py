6e9021c4c9f994e46a4682e8155a928729af995a
Cedric Travelletti <cedrictravelletti@gmail.com>
2019-03-07 16:53:26 +0100

    Working example of gradient descent in Torch.

--- volcapy/cloud/run_mesh_distance.py
+++ volcapy/cloud/run_mesh_distance.py
@@ -9,6 +9,9 @@ import volcapy.grid.covariance_tools as cl
 m0 = 2200.0
 data_std = 0.1
 
+length_scale = 100.0
+sigma = 20.0
+
 
 # Initialize an inverse problem from Niklas's data.
 # This gives us the forward and the coordinates of the inversion cells.
@@ -31,45 +34,84 @@ torch.set_num_threads(4)
 distance_mesh = torch.as_tensor(distance_mesh)
 F = torch.as_tensor(inverseProblem.forward)
 
-m_prior = torch.mul(m0, torch.ones((inverseProblem.n_model, 1)))
 data_cov = torch.mul(data_std**2, torch.eye(inverseProblem.n_data))
 
 d_obs = torch.as_tensor(inverseProblem.data_values)
 
 
-def posterior_squared_exponential(distance_mesh, inv_lambda_2, sigma_2):
-    """ Squared exponential kernel. Builds the full covariance matrix from a
-    squared distance mesh.
+class SquaredExpModel(torch.nn.Module):
+    def __init__(self):
+        super(SquaredExpModel, self).__init__()
 
-    Parameters
-    ----------
-    lambda_2: float
-        Length scale parameter, in the form 1(2 * lambda^2).
-    sigma_2: float
-        (Square of) standard deviation.
+        self.m0 = torch.nn.Parameter(torch.tensor(m0))
+        self.length_scale = torch.nn.Parameter(torch.tensor(length_scale))
+        self.sigma = torch.nn.Parameter(torch.tensor(sigma))
+        
+        self.m_prior = torch.mul(self.m0, torch.ones((inverseProblem.n_model, 1)))
 
-    """
-    pushforward_cov = (torch.mm(
-            torch.mul(
-            torch.exp(torch.mul(distance_mesh, - inv_lambda_2)),
-            sigma_2),
-            F.t()
-            ))
-    inversion_operator = torch.inverse(
-            torch.add(
-                    data_cov,
-                    torch.mm(F, pushforward_cov)
-                    ))
-    m_posterior = torch.add(
-            m_prior,
-            torch.mm(
-                    torch.mm(pushforward_cov, inversion_operator),
-                    torch.sub(d_obs, torch.mm(F, m_prior)))
-            )
-    return m_posterior
+    def forward(self, distance_mesh):
+        """ Squared exponential kernel. Builds the full covariance matrix from a
+        squared distance mesh.
+    
+        Parameters
+        ----------
+        lambda_2: float
+            Length scale parameter, in the form 1(2 * lambda^2).
+        sigma_2: float
+            (Square of) standard deviation.
+    
+        """
+        pushforward_cov = (torch.mm(
+                torch.mul(
+                    torch.exp(
+                            torch.mul(distance_mesh,
+                                    - 1/(2 * self.length_scale.pow(2)))
+                            ),
+                self.sigma.pow(2)),
+                F.t()
+                ))
+        inversion_operator = torch.inverse(
+                torch.add(
+                        data_cov,
+                        torch.mm(F, pushforward_cov)
+                        ))
+        m_posterior = torch.add(
+                self.m_prior,
+                torch.mm(
+                        torch.mm(pushforward_cov, inversion_operator),
+                        torch.sub(d_obs, torch.mm(F, self.m_prior)))
+                )
+        return torch.mm(F, m_posterior)
 
-# Matrix multiplication.
-c = torch.mm(F, a)
 
-# Elementwise exponential (underscore means in-place).
-a = a.exp_()
+myModel = SquaredExpModel()
+criterion = torch.nn.MSELoss() 
+optimizer = torch.optim.SGD(myModel.parameters(), lr = 0.5) 
+  
+for epoch in range(10): 
+  
+    # Forward pass: Compute predicted y by passing  
+    # x to the model 
+    pred_y = myModel(distance_mesh) 
+  
+    # Compute and print loss 
+    loss = criterion(pred_y, d_obs) 
+  
+    # Zero gradients, perform a backward pass,  
+    # and update the weights. 
+    optimizer.zero_grad() 
+    loss.backward() 
+    optimizer.step() 
+    print(
+            'epoch {}, loss {}, m0 {} length_scale {}, sigma {}'.format(
+                    epoch, loss.data,
+                    float(myModel.m0),
+                    float(myModel.length_scale),
+                    float(myModel.sigma)
+                    )) 
+  
+"""
+new_var = Variable(torch.Tensor([[4.0]])) 
+pred_y = our_model(new_var) 
+print("predict (after training)", 4, our_model(new_var).data[0][0]) 
+"""
