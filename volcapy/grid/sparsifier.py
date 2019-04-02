32a4be92e542a12a937408f033f1b3d5f4420467
Cedric Travelletti <cedrictravelletti@gmail.com>
2019-04-02 10:30:32 +0200

    Parallelized sparsifier.

--- volcapy/grid/sparsifier.py
+++ volcapy/grid/sparsifier.py
@@ -6,6 +6,8 @@ euclidean distance between the points.
 """
 import numpy as np
 from sklearn.neighbors import BallTree
+from multiprocessing import Pool
+from itertools import repeat
 
 
 class Sparsifier():
@@ -38,7 +40,7 @@ class Sparsifier():
         return self.tree.query_radius(cell.reshape(1, -1), radius)[0]
 
     def compute_sparse_distance_mesh(self, threshold_dist):
-        """ Compute the n_model * n_model matrix of squared euclidean distances
+        """ Compute the n_model * n_model matrix of euclidean distances
         between all couples, ignoring couples with a distance greather than
         *threshold_dist*.
 
@@ -49,35 +51,78 @@ class Sparsifier():
 
         Returns
         -------
-        squared_dists: List[float]
-            List of squared distances between couples of cells.
-        inds: List[int]
+        dists: List[float]
+            List of euclidean distances between couples of cells.
+        inds: List[(int, int)]
             Gives the indices inside the full matrix, of the values in the
             preceding list.
 
         """
+        pool = Pool()
+        # Loop (in parallel) over all cells (here corresponds to looping ove
+        # rows of the covariace/mesh matrix.
+        # For each cell/row, only consider elements of the covariance/mesh
+        # matrix that are within a certain threshold_dist.
+
+        # The code below is a little bit involved, since it uses
+        # multiprocessing. Python multiprocessing only allows to loop in
+        # parallele over a single list and return a single list.
+        # We thus have to unpack the output using the zip(* ) idiom and to pack
+        # the inputs using zip and the itertools.repeat() to give the same
+        # threshold_dist to each process.
+
+        # Finally, since a single function call (on on cell) already returns a
+        # list, we will get lists of lists. We unpack them using
+        # itertools.chain.
+        dists, inds = zip(
+            *pool.starmap(self.per_cell_dist_mesh,
+                    zip(self.inverseProblem.cells_coords,
+                            list(range(self.inverseProblem.n_model)),
+                            repeat(threshold_dist))))
+        # Free up resources.
+        pool.close()
+        pool.join()
+
+        # Un-nest the lists.
+        dists = list(itertools.chain.from_iterable(dists))
+        ind = list(itertools.chain.from_iterable(ind))
+
+        # Return numpy array, so TF doesnt have to cast later.
+        return (np.array(dists), np.array(inds))
+
+    def per_cell_dist_mesh(self, cell, cell_ind, threshold_dist):
+        """ Helper function for the above, so we can parallelize.
+
+        Parameters
+        ----------
+        cell: Cell
+            Cells to consider in the covariance matrix.
+        cell_ind: int
+            Index of the cell in the covariance matrix (its row number).
+
+        Returns
+        -------
+
+        """
         # Lists to store the computed squared_dists and indices in the big sparse
         # matrix.
-        squared_dists = []
+        dists = []
         inds = []
 
-        # Loop over all cells.
-        for cell_ind, cell in enumerate(self.inverseProblem.cells_coords):
-            print("BallTree processing cell nr: {}".format(cell_ind))
+        print("BallTree processing cell nr: {}".format(cell_ind))
 
-            # Get neighbors.
-            neighbor_inds = self.get_cells_ind_with_radius(cell, threshold_dist)
+        # Get neighbors.
+        neighbor_inds = self.get_cells_ind_with_radius(cell, threshold_dist)
 
-            # We always return at least the cell itself.
-            if len(neighbor_inds) == 0:
-                neighbor_inds = [cell_ind]
+        # We always return at least the cell itself.
+        if len(neighbor_inds) == 0:
+            neighbor_inds = [cell_ind]
 
-            # Loop over all neighboring cells.
-            for neighbor_ind in neighbor_inds:
-                squared_dists.append(
-                    np.float32(
-                        np.linalg.norm(
-                            cell - self.inverseProblem.cells_coords[neighbor_ind, :])))
-                inds.append((cell_ind, neighbor_ind))
-        # Return numpy array, so TF doesnt have to cast later.
-        return (np.array(squared_dists), np.array(inds))
+        # Loop over all neighboring cells.
+        for neighbor_ind in neighbor_inds:
+            dists.append(
+                np.float32(
+                    np.linalg.norm(
+                        cell - self.inverseProblem.cells_coords[neighbor_ind, :])))
+            inds.append((cell_ind, neighbor_ind))
+        return (dists, inds)
