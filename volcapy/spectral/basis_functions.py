1a0a512a1759532d6d6389bc7b880f3fa2f6068a
EC2 Default User <ec2-user@ip-172-31-4-211.eu-west-1.compute.internal>
2019-04-05 12:43:07 +0000

    Basis functions now runs on CUDA.

--- volcapy/spectral/basis_functions.py
+++ volcapy/spectral/basis_functions.py
@@ -10,16 +10,16 @@ import torch
 
 
 # GLOBALS
-m0 = 2200.0
-data_std = 0.1
+m0 = torch.tensor(2200.0, requires_grad=False).cuda()
+data_std = torch.tensor(0.1, requires_grad=False).cuda()
 sigma_epsilon = data_std
-sigma_0 = 1.0
+sigma_0 = torch.tensor(50.0, requires_grad=False).cuda()
 
-n_basis = 1000
-n_dims = 3
+n_basis = 1500
+n_dims = torch.tensor(3, requires_grad=False).cuda()
 
 # Initial length scales.
-lambda0 = 300**2
+lambda0 = torch.tensor(2 * 800**2, requires_grad=False).cuda()
 length_scales_init = lambda0 * torch.ones((n_basis, 1), dtype=torch.float32).cuda()
 
 # Initialize an inverse problem from Niklas's data.
@@ -32,7 +32,7 @@ inverseProblem = InverseProblem.from_matfile(niklas_data_path)
 
 # Subset the data and build the train/test datasets.
 F_test, d_obs_test = inverseProblem.subset_data(500)
-n_model = inverseProblem.n_model
+n_model = torch.tensor(inverseProblem.n_model, requires_grad=False).cuda()
 
 F_train = torch.as_tensor(inverseProblem.forward).cuda()
 # Careful: we have to make a column vector here.
@@ -57,10 +57,10 @@ class SquaredExpModel(torch.nn.Module):
     def __init__(self):
         super(SquaredExpModel, self).__init__()
 
-        self.m0 = torch.nn.Parameter(torch.tensor(m0)).cuda()
-        self.sigma_0 = torch.nn.Parameter(torch.tensor(sigma_0)).cuda()
+        self.m0 = torch.nn.Parameter(m0).cuda()
+        self.sigma_0 = torch.nn.Parameter(sigma_0).cuda()
 
-        self.m_prior = torch.mul(self.m0, torch.ones((inverseProblem.n_model, 1))).cuda()
+        self.m_prior = torch.mul(self.m0, torch.ones((n_model, 1)).cuda()).cuda()
         self.data_cov = data_cov.cuda()
 
         # Initial lengthscales
@@ -83,11 +83,12 @@ class SquaredExpModel(torch.nn.Module):
         prior_misfit = torch.sub(d_obs_train, torch.mm(F_train, self.m_prior))
 
         # Compute C_M GT.
-        pushforward_cov = torch.mul(sigma_0**2,
+        pushforward_cov = torch.mul(self.sigma_0**2,
                 torch.mm(
                     self.PHI.t(),
                     torch.mm(self.PHI, F_train.t()))
                 )
+        print(pushforward_cov)
         inv_inversion_operator = torch.add(
                         self.data_cov,
                         torch.mm(F_train, pushforward_cov)
@@ -95,9 +96,8 @@ class SquaredExpModel(torch.nn.Module):
         inversion_operator = torch.inverse(inv_inversion_operator)
 
         # Need to do it this way, otherwise rounding errors kill everything.
-        log_det = - torch.logdet(inversion_operator)
-
-        prior_misfit = torch.sub(d_obs_train, torch.mm(F_train, self.m_prior))
+        log_det = torch.logdet(inv_inversion_operator)
+        print(log_det)
 
         m_posterior = torch.add(
                 self.m_prior,
@@ -113,36 +113,35 @@ class SquaredExpModel(torch.nn.Module):
                       prior_misfit.t(),
                       torch.mm(inversion_operator, prior_misfit)))
 
+        # return log_likelyhood
         return log_likelyhood
 
 
 myModel = SquaredExpModel()
-myModel.cuda()
+myModel = myModel.cuda()
 
-optimizer = torch.optim.Adam(myModel.parameters(), lr=1.3)
-criterion = torch.nn.MSELoss()
+optimizer = torch.optim.Adam(myModel.parameters(), lr=5.0)
+criterion = torch.nn.MSELoss().cuda()
 
 for epoch in range(100):
 
     # Forward pass: Compute predicted y by passing
     # x to the model
-    log_likelyhood = myModel()
+    output = myModel().cuda()
 
     # Compute and print loss
-    loss = log_likelyhood
-    print(loss)
+    # loss = criterion(torch.mm(F_train, output), d_obs_train)
+    loss = output
 
     # Zero gradients, perform a backward pass,
     # and update the weights.
-    """
     optimizer.zero_grad()
     loss.backward(retain_graph=True)
     optimizer.step()
     print(
             'epoch {}, loss {}, m0 {} length_scales {}, sigma {}'.format(
-                    epoch, loss.data,
+                    epoch, loss,
                     float(myModel.m0),
                     myModel.length_scales[0],
                     float(myModel.sigma_0)
                     ))
-    """
