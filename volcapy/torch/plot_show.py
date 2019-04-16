611a1dedbc2ef9d7d8284facba3363877b41df5b
Cedric Travelletti <cedrictravelletti@gmail.com>
2019-04-16 20:12:44 +0200

    Nice contour plotting.

--- volcapy/torch/plot_show.py
+++ volcapy/torch/plot_show.py
@@ -11,3 +11,14 @@ sigma0s = np.load("sigma0s_contour.npy")
 plt.contour(sigma0s, m0s, lls)
 plt.contour(sigma0s, m0s, train_rmses)
 plt.show()
+
+plot = plt.contour(sigma0s, m0s, lls, levels=[-1200, -1180, -1171, -1170,
+        -1160, -1150, -1000, 0, 500, 1000, 4000])
+plt.clabel(plot, inline=1, fontsize=7)
+# plt.contourf(sigma0s, m0s, train_rmses)
+plt.show()
+
+plot = plt.contour(sigma0s, m0s, train_rmses, levels=[0.1, 0.2,
+        0.5])
+plt.clabel(plot, inline=1, fontsize=7)
+plt.show()
