c947ebca1ae5398a8fca818812ed4600984fece9
Cedric Travelletti <cedrictravelletti@gmail.com>
2019-04-24 09:13:22 +0200

    Plotting params evolution for multiple lambda0s.

--- volcapy/plotting/plotting_example.py
+++ volcapy/plotting/plotting_example.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 from volcapy.inverse.flow import InverseProblem
 
 
-path = "/home/cedric/PHD/Dev/Volcano/data/Cedric.mat"
-niklas_data_path = "/home/cedric/PHD/Dev/Volcano/data/Cedric.mat"
-data = ld.load_niklas(path)
+# niklas_data_path = "/home/cedric/PHD/Dev/Volcano/data/Cedric.mat"
+niklas_data_path = "/idiap/temp/ctravelletti/tflow/Volcano/data/Cedric.mat"
+data = ld.load_niklas(niklas_data_path)
 inverseProblem = InverseProblem.from_matfile(niklas_data_path)
 coords = data['coords'].astype(dtype=np.float32, order='C', copy=False)
 
@@ -54,4 +54,40 @@ axarr[2].axvline(x=2943.0)
 axarr[2].legend()
 plt.xlabel("Training epoch")
 plt.title("Evolution during training, fixed lambda0 = 200.0m, Test/Train split: 450/102")
+plt.show()
+
+# Training Evolution of Train/Test RMSE for multiple lambdas.
+lambda0s = np.load("/idiap/temp/ctravelletti/tflow/Volcano/volcapy/torch/presentation/lambda0s_train.npy")
+lls = np.load("/idiap/temp/ctravelletti/tflow/Volcano/volcapy/torch/presentation/log_likelihoods_train.npy")
+m0s = np.load("/idiap/temp/ctravelletti/tflow/Volcano/volcapy/torch/presentation/m0s_train.npy")
+sigma0s = np.load("/idiap/temp/ctravelletti/tflow/Volcano/volcapy/torch/presentation/sigma0s_train.npy")
+test_rmses = np.load("/idiap/temp/ctravelletti/tflow/Volcano/volcapy/torch/presentation/test_rmses_train.npy")
+train_rmses = np.load("/idiap/temp/ctravelletti/tflow/Volcano/volcapy/torch/presentation/train_rmses_train.npy")
+
+# Discard the ones with nan log-likelihood.
+non_nan_inds = list(range(9)) + [12]
+# Plot test RMSES.
+for i in non_nan_inds:
+    plt.plot(test_rmses[i, :], label="lambda0 = {}".format(lambda0s[i]))
+plt.legend()
+plt.ylim((0.0, 2.0))
+plt.show()
+
+# Plot params RMSES.
+f, axarr = plt.subplots(4, sharex=True)
+axarr[0].set_title("Evolution of parameters and test RMSE along training for multiple lambda0s. Train/Test: 450/102.")
+for i in non_nan_inds:
+    axarr[0].plot(m0s[i, :], label="lambda0 = {}".format(lambda0s[i]))
+    axarr[0].set_xlabel("m0")
+
+    axarr[1].plot(sigma0s[i, :], label="lambda0 = {}".format(lambda0s[i]))
+    axarr[1].set_xlabel("sigma0")
+
+    axarr[2].plot(test_rmses[i, :], label="lambda0 = {}".format(lambda0s[i]))
+    axarr[2].set_xlabel("test_rmse")
+    axarr[2].set_ylim((0.0, 2.0))
+
+    axarr[3].plot(lls[i, :], label="lambda0 = {}".format(lambda0s[i]))
+    axarr[2].set_xlabel("log-likelihood")
+plt.legend()
 plt.show()
