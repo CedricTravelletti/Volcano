9fb5e91dffdabc160397df1daf52d412c7690e75
Cedric Travelletti <cedrictravelletti@gmail.com>
2019-06-27 16:03:04 +0200

    [Refac] Separated plottig examples for: final hyperparams and erros, evolution of those along training and 3d posterior.

--- volcapy/run_examples/plot_final_perlambd0.py
+++ volcapy/run_examples/plot_final_perlambd0.py
@@ -1,5 +1,5 @@
-""" Plot evolution of test / train RMSE for different lambda0s.
-This can be used to visualize results when doing brute for search on lambda0.
+""" For each lambda0 (in the grid search) plot the final train RMSE, LOOCV RMSE
+and hyperparams.
 
 """
 import volcapy.loading as ld
@@ -12,22 +12,25 @@ from volcapy.inverse.flow import InverseProblem
 LOAD
 """
 # input_folder = "/idiap/temp/ctravelletti/out/simple_exponential/"
-input_folder = "/home/cedric/PHD/run_results/matern52/"
+input_folder = "/home/cedric/PHD/run_results/last/"
 
-# Training Evolution of Train/Test RMSE for multiple lambdas.
 lambda0s = np.load(os.path.join(input_folder, "lambda0s_train.npy"))
 lls = np.load(os.path.join(input_folder, "log_likelihoods_train.npy"))
 m0s = np.load(os.path.join(input_folder, "m0s_train.npy"))
 sigma0s = np.load(os.path.join(input_folder, "sigma0s_train.npy"))
-test_rmses = np.load(os.path.join(input_folder, "test_rmses_train.npy"))
 train_rmses = np.load(os.path.join(input_folder, "train_rmses_train.npy"))
+loocv_rmses = np.load(os.path.join(input_folder, "loocv_rmses_train.npy"))
 
-# Plot Evolution
+# Plot train and LOOCV final error for each lambda0.
 plt.figure()
 plt.subplot(211)
 plt.title("Train/Test split: 450/103.")
-plt.plot(lambda0s, train_rmses, "r*", label="Train")
-plt.plot(lambda0s, test_rmses, "bo", label="Test")
+plt.plot(lambda0s, train_rmses, "r*", label="Train", markersize=12)
+plt.plot(lambda0s, loocv_rmses, "bo", label="LOOCV RMSE")
+
+# Have to add limits because of diverged trainings.
+plt.ylim((0, 3.0))
+# plt.ylim((0, 40.0))
 plt.xlabel("lambda0 [m]")
 plt.ylabel("RMSE [mGal], std=0.1")
 plt.axhline(0.1, color='r')
@@ -40,13 +43,14 @@ plt.ylabel("Log-likelihood (shifted)")
 plt.show()
 
 
-# Plot Parameters
+# Plot final hyperparameters for each lambda0.
 plt.figure()
 plt.subplot(211)
 plt.title("Train/Test split: 450/103.")
 plt.plot(lambda0s, m0s, "r*", label="m0")
 plt.xlabel("lambda0 [m]")
 plt.ylabel("m0 [kg/m3]")
+plt.ylim((1800, 3000))
 plt.axhline(0.1, color='r')
 plt.legend()
 
---
+++ volcapy/run_examples/plot_posterior_3d.py
@@ -0,0 +1,111 @@
+""" Presentation for meeting with Niklas 13 February.
+
+"""
+from volcapy.loading import load_niklas
+import volcapy.plotting.plot as plt
+import volcapy.uq.azzimonti as azz
+
+import numpy as np
+
+
+# posterior_mean_path = "/home/cedric/PHD/Dev/Volcano/results/m_posterior.npy"
+posterior_mean_path = "/home/cedric/PHD/run_results/forwards/m_post_200_matern.npy"
+posterior_mean_path2 = "/home/cedric/PHD/run_results/forwards/m_post_250.0_sqexp.npy"
+
+"""
+posterior_mean_path = "/home/cedric/PHD/Dev/Volcano/results/m_posterior.npy"
+posterior_variance_path = "/home/cedric/PHD/Dev/Volcano/results/posterior_cov_diag.npy"
+"""
+niklas_data_path = "/home/cedric/PHD/Dev/Volcano/data/Cedric.mat"
+
+# Load inversion results.
+post_mean = np.load(posterior_mean_path)
+post_mean2 = np.load(posterior_mean_path2)
+post_variance = np.load(posterior_variance_path)
+
+# Load data from Niklas (forward and measurements).
+niklas_data = load_niklas(niklas_data_path)
+F = niklas_data['F']
+coords = niklas_data['coords']
+data_values = niklas_data['d']
+data_coords = niklas_data['data_coords']
+
+# ---------------------
+# Inversion parameters: mean = 2350, lambda = sigma_2 = 50.0**2, lambda_2 =
+# 130**2. Data noise = 0.1**2.
+# ---------------------
+
+# Island boundaries.
+island_minx = 516000.0
+island_maxx = 522000.0
+island_miny = 4291000.0
+island_maxy = 4296000.0
+
+# Find the indices of the cells within the island.
+island_inds = np.where(
+        (coords[:, 0] > island_minx)
+        & (coords[:, 0] < island_maxx)
+        & (coords[:, 1] > island_miny)
+        & (coords[:, 1] < island_maxy))[0]
+
+# Out of these, find the surface cells (roughly, those are the first 32000).
+surface_inds = island_inds[island_inds < 32000]
+
+# Plot surface.
+plt.plot_region(surface_inds, post_mean.reshape(-1), coords, cmin=2250, cmax=2550.0)
+plt.plot_region(surface_inds, post_mean2.reshape(-1), coords, cmin=2100, cmax=2550.0)
+
+plt.plot_region(island_inds, post_mean2.reshape(-1), coords, cmin=2200,
+        cmax=2500.0, n_sample=10000)
+
+plt.plot(post_mean2.reshape(-1), coords, cmin=2100, cmax=2550.0, n_sample=50000)
+
+plt.plot_z_slice([-500, 0, 500], post_mean.reshape(-1), coords, cmin=1800, cmax=2550.0)
+plt.plot_z_slice([-500, 0, 500], post_mean2.reshape(-1), coords)
+
+# Plot slices as in Niklas.
+plt.plot_z_slice(0.0, post_mean,
+        coords[:, 0],coords[:, 1],coords[:, 2])
+
+plt.plot_z_slice(500.0, post_mean,
+        coords[:, 0],coords[:, 1],coords[:, 2])
+
+plt.plot_z_slice(800.0, post_mean,
+        coords[:, 0],coords[:, 1],coords[:, 2])
+
+plt.plot_z_slice(-100.0, post_mean,
+        coords[:, 0],coords[:, 1],coords[:, 2])
+
+# Plot excursion set plug-in estimate.
+excursion_inds = np.where(post_mean > 2500.0)
+plt.plot_region(excursion_inds, post_mean,
+        coords[:, 0],coords[:, 1],coords[:, 2])
+
+# Plot residual standard deviation.
+post_std_dev = np.sqrt(post_variance)
+plt.plot_region(island_inds, post_std_dev,
+        coords[:, 0],coords[:, 1],coords[:, 2])
+
+# Plot a slice of the residual std at sea level.
+plt.plot_z_slice(0.0, post_std_dev,
+        coords[:, 0],coords[:, 1],coords[:, 2])
+
+# Plot individual measurement.
+# Index of the measurement we want to consider.
+data_index = 200
+
+my_coords = np.vstack((coords, data_coords[data_index]))
+
+my_mean = np.append(post_mean, 100000)
+
+plt.plot_region(my_surface_inds, my_mean, my_coords[:,0], my_coords[:,1],
+        my_coords[:,2])
+
+
+# Azzimonti Stuff
+mygp = azz.GaussianProcess(post_mean, post_variance, covariance_func=None)
+excu = mygp.compute_excursion_probs(threshold=2500.0)
+
+# Vorobev expectation.
+vorb_inds = mygp.vorobev_expectation_inds(threshold=2500.0)
+plt.plot_region(vorb_inds, excu, coords[:,0], coords[:,1], coords[:,2])
