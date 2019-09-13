75b140f63d95cd9f836b5b064d564952d80bfb4c
Cedric Travelletti <cedrictravelletti@gmail.com>
2019-09-13 17:38:02 +0200

    Building of synthetic grid works.

--- volcapy/synthetic/grid.py
+++ volcapy/synthetic/grid.py
@@ -4,9 +4,10 @@ cells_coords: ndarray, shape n_cells * n_dims
 
 """
 import numpy as np
+from volcapy.niklas.banerjee import banerjee
 
 
-def buil_cube(nr_x, res_x, nr_y, res_y, nr_z, res_z):
+def build_cube(nr_x, res_x, nr_y, res_y, nr_z, res_z):
     """ Builds a gridded cube.
 
     Parameters
@@ -28,17 +29,126 @@ def buil_cube(nr_x, res_x, nr_y, res_y, nr_z, res_z):
 
     """
     # Make sure first cell starts at [0, 0, 0]
-    current_x = res_x / 2.0
-    current_y = res_y / 2.0
-    current_z = res_z / 2.0
+    orig_x = res_x / 2.0
+    orig_y = res_y / 2.0
+    orig_z = res_z / 2.0
 
     coords = []
 
+    current_x = orig_x
     for i in range(nr_x):
+        current_y = orig_y
         for j in range(nr_y):
+            current_z = orig_z
             for k in range(nr_z):
                 coords.append([current_x, current_y, current_z])
                 current_z += res_z
             current_y += res_y
         current_x += res_x
     return np.array(coords)
+
+def compute_forward(coords, res_x, res_y, res_z, data_coords):
+    """ Compute forward operator.
+
+    Parameters
+    ----------
+    coords: ndarray
+        Cells centroid coordinates, size n_cell * n_dims.
+    res_x: float
+        Length of a cell in x-direction (meters).
+    res_y_float
+    res_z: float
+    data_coords: ndarray
+        List of data measurements coordinates, size n_data * n_dims.
+
+    Returns
+    -------
+    ndarray
+        Forward operator, size n_data * n_cells.
+
+    """
+    n_cells = coords.shape[0]
+    n_data = data_coords.shape[0]
+    F = np.zeros((n_data, n_cells))
+
+    for i, cell in enumerate(coords):
+        for j, data in enumerate(data_coords):
+            # Compute cell endpoints.
+            xh = cell[0] + res_x / 2.0
+            xl = cell[0] - res_x / 2.0
+            yh = cell[1] + res_y / 2.0
+            yl = cell[1] - res_y / 2.0
+            zh = cell[2] + res_z / 2.0
+            zl = cell[2] - res_z / 2.0
+            
+            F[j, i] = banerjee(
+                    xh, xl, yh, yl, zh, zl,
+                    data[0],  data[1],  data[2])
+    return F
+
+def generate_regular_surface_datapoints(
+        xl, xh, nx, yl, yh, ny, zl, zh, nz,
+        offset):
+    """ Put regularly spaced measurement points on the surface of a
+    cube.
+    Note that there will always be measurement sites at the endpoints of the
+    cube.
+    We need an offset because measerements cannot be directly on the endpoints
+    of a cell because of division by zero in the Bannerjee formula.
+
+    Parameters
+    ----------
+    xl: float
+        Lower x-coordinate of the cube.
+    xh: float
+        Higher x-coordinate of the cube.
+    nx: int
+        Number of measurments in x-dimension.
+    yl: float
+    yh: float
+    ny: int
+    zl: float
+    zh: float
+    nz: int
+    offset: float
+        Displace the measurements sites by an offset outside of the cube to
+        avoid division by zero.
+
+    Returns
+    -------
+    ndarray
+        Coordinates of the measurement sites, size n_data * n_dims.
+
+    """
+    data_coords = []
+
+    # Bottom x surface.
+    for y in np.linspace(yl - offset, yh + offset, ny):
+        for z in np.linspace(zl - offset, zh + offset, nz):
+            data_coords.append([xl - offset, y, z])
+
+    # Top x surface.
+    for y in np.linspace(yl - offset, yh + offset, ny):
+        for z in np.linspace(zl - offset, zh + offset, nz):
+            data_coords.append([xh + offset, y, z])
+
+    # Bottom y surface.
+    for x in np.linspace(xl - offset, xh + offset, nx):
+        for z in np.linspace(zl - offset, zh + offset, nz):
+            data_coords.append([x, yl - offset, z])
+
+    # Top y surface.
+    for x in np.linspace(xl - offset, xh + offset, nx):
+        for z in np.linspace(zl - offset, zh + offset, nz):
+            data_coords.append([x, yh + offset, z])
+
+    # Bottom z surface.
+    for x in np.linspace(xl - offset, xh + offset, nx):
+        for y in np.linspace(yl - offset, yh + offset, ny):
+            data_coords.append([x, y, zl - offset])
+
+    # Top z surface.
+    for x in np.linspace(xl - offset, xh + offset, nx):
+        for y in np.linspace(yl - offset, yh + offset, ny):
+            data_coords.append([x, y, zh + offset])
+    return np.array(data_coords)
