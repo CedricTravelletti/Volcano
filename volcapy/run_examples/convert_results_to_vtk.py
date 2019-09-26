a3231742a137f7f4201a4444fc50307c4d1f7812
Cedric Travelletti <cedrictravelletti@gmail.com>
2019-09-26 10:50:36 +0200

    Convert volcano inversion results to VTK.

--- volcapy/run_examples/convert_results_to_vtk.py
+++ volcapy/run_examples/convert_results_to_vtk.py
@@ -1,7 +1,7 @@
 from volcapy.inverse.inverse_problem import InverseProblem
 from volcapy.inverse.gaussian_process import GaussianProcess
 from volcapy.compatibility_layer import match_grids, get_regularization_cells_inds
-from volcapy.synthetic.vtkutils import ndarray_to__vtk
+from volcapy.synthetic.vtkutils import ndarray_to_vtk
 import volcapy.covariance.matern32 as cl
 
 import numpy as np
@@ -14,7 +14,7 @@ inverseProblem = InverseProblem.from_matfile(niklas_data_path)
 
 
 # Load inversion results.
-posterior_mean_path = "/home/cedric/PHD/run_results/forwards/m_post_622_sqexp.npy"
+posterior_mean_path = "/home/cedric/PHD/run_results/forwards/m_post_562_sqexp.npy"
 m_post_m = np.load(posterior_mean_path)
 
 # SPECIAL: Remove regularisation cells.
@@ -38,4 +38,4 @@ m_post_reg[reg_inds] = m_post_m
 m_post_reg = m_post_reg.reshape(nx, ny, nz)
 
 ndarray_to_vtk(m_post_reg, res_x, res_y, res_z,
-            "reconstructed_density.mhd")
+            "reconstructed_density_cholesky.mhd")
