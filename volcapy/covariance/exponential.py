Fixed simple exponential kernel. Matern32 can now compute FULL covariance matrix.

volcapy/covariance/exponential.py
@@ -5,8 +5,11 @@ IMPORTANT: Note that we always strip the variance parameter sigma0 from the
 covariance matrix. Hence, when using the covariance pushforward computed here,
 one has to manually multiply by sigma0^2 for expressions to make sense.
 
+THIS ONE COMPUTES THE SIMPLE EXPONENTIAL (LAPLACE).
+
 """
 import torch
+import numpy as np
 
 # General torch settings and devices.
 torch.set_num_threads(8)
@@ -51,13 +54,12 @@ def compute_cov_pushforward(lambda0, F, cells_coords, device, n_chunks=200,
     """
     start = timer()
 
-
     # Transfer everything to device.
     lambda0 = torch.tensor(lambda0, requires_grad=False).to(device)
     F = F.to(device)
     cells_coords = cells_coords.to(device)
 
-    inv_lambda2 = - 1 / (2 * lambda0**2)
+    inv_lambda2 = - 1 / lambda0
     n_dims = 3
     n_model = F.shape[1]
 
@@ -74,15 +76,15 @@ def compute_cov_pushforward(lambda0, F, cells_coords, device, n_chunks=200,
             torch.cuda.synchronize()
             torch.cuda.empty_cache()
 
-        # Squared euclidean distance.
-        d_2 = torch.sqrt(torch.pow(
+        # Euclidean distance.
+        d = torch.sqrt(torch.pow(
             x.unsqueeze(1).expand(x.shape[0], n_model, n_dims)
             - cells_coords.unsqueeze(0).expand(x.shape[0], n_model, n_dims)
             , 2).sum(2))
         tot = torch.cat((
                 tot,
                 torch.matmul(
-                    torch.exp(inv_lambda2 * d_2)
+                    torch.exp(inv_lambda2 * d)
                     , F.t())))
 
     # Wait for all threads to complete.