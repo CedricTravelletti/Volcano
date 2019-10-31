97e79086a9e3df4978ee6aefb3870b547c70d634
Cedric Travelletti <cedrictravelletti@gmail.com>
2019-10-31 18:53:57 +0100

    Made covariance a module.

--- volcapy/covariance/__init__.py
+++
@@ -1,8 +0,0 @@
-""" Tools to compute covariance matrix (data side) and covariance pushforward,
-on GPU.
-
-IMPORTANT: Note that we always strip the variance parameter sigma0 from the
-covariance matrix. Hence, when using the covariance pushforward computed here,
-one has to manually multiply by :math:`\sigma_0^2:: for expressions to make sense.
-
-"""
