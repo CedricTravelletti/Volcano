e53b2b49f2f48f0e0c4bd549d0c0f95ec6e80d0d
Cedric Travelletti <cedrictravelletti@gmail.com>
2019-10-31 11:38:22 +0100

    Now importing submodules in subpackage init, like in gpytorch. This eases documentation.

---
+++ volcapy/inverse/__init__.py
@@ -0,0 +1,10 @@
+from .inverse_problem import InverseProblem
+from .gaussian_process import GaussianProcess
+from .random_spatial_effects import SREModel
+
+
+__all__ = [
+        "InverseProblem",
+        "GaussianProcess",
+        "SREModel",
+]
