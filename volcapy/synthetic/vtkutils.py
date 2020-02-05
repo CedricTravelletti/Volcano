f81a9a2d2a763e8a2b33623a9654a2ec5f7a928b
Cedric Travelletti <cedrictravelletti@gmail.com>
2020-02-05 16:02:07 +0100

    The conversion of synthetic data to VTK was wrong. Fixed now.

--- volcapy/synthetic/vtkutils.py
+++ volcapy/synthetic/vtkutils.py
@@ -23,16 +23,25 @@ def save_vtk(data, shape, res_x, res_y, res_z, filename):
     # using the *Threshold* filter in Paraview.
     data[data<=0.0] = np.nan
 
-    data = data.reshape(shape, order="F")
+    # Checks
+    print("Data shape {}".format(data.shape))
+    print("Target shape {}".format(shape))
+    print("Target shape (total) {}".format(shape[0]*shape[1]*shape[2]))
+
+    # data = data.reshape(shape, order="F")
+    # VTK uses strange ordering (z first) so have to transpose data.
+    data = data.reshape(shape, order="C") # Back to 3d.
 
     # vtkImageData is the vtk image volume type
     imdata = vtk.vtkImageData()
-    # this is where the conversion happens
-    depthArray = numpy_support.numpy_to_vtk(data.ravel(), deep=True,
+    # This is where the conversion happens.
+    # Pay attention, VTK uses Fortran-like ordering, where first index loops
+    # fastest.
+    depthArray = numpy_support.numpy_to_vtk(data.ravel(order='F'), deep=True,
             array_type=vtk.VTK_DOUBLE)
     
     # fill the vtk image data object
-    imdata.SetDimensions(data.shape)
+    imdata.SetDimensions(shape)
     imdata.SetSpacing([res_x,res_y,res_z])
     imdata.SetOrigin([0,0,0])
     imdata.GetPointData().SetScalars(depthArray)
@@ -60,13 +69,35 @@ def ndarray_to_vtk(data, res_x, res_y, res_z, filename):
     data[data<=0.0] = np.nan
 
     # See the TRANSPOSE? VTK uses strange ordering.
+    """
+    vtk_data_array = numpy_support.numpy_to_vtk(
+                num_array=data.ravel(),
+                deep=True, array_type=vtk.VTK_FLOAT)
+    """
     vtk_data_array = numpy_support.numpy_to_vtk(
                 num_array=data.transpose(2, 1, 0).ravel(),
                 deep=True, array_type=vtk.VTK_FLOAT)
+    """
+    vtk_data_array = numpy_support.numpy_to_vtk(
+                num_array=data.transpose(0, 2, 1).ravel(),
+                deep=True, array_type=vtk.VTK_FLOAT)
+    vtk_data_array = numpy_support.numpy_to_vtk(
+                num_array=data.transpose(1, 2, 0).ravel(),
+                deep=True, array_type=vtk.VTK_FLOAT)
+    vtk_data_array = numpy_support.numpy_to_vtk(
+                num_array=data.transpose(2, 0, 1).ravel(),
+                deep=True, array_type=vtk.VTK_FLOAT)
+    vtk_data_array = numpy_support.numpy_to_vtk(
+                num_array=data.transpose(1, 0, 2).ravel(),
+                deep=True, array_type=vtk.VTK_FLOAT)
+    vtk_data_array = numpy_support.numpy_to_vtk(
+                num_array=data.transpose(0, 1, 2).ravel(),
+                deep=True, array_type=vtk.VTK_FLOAT)
+    """
 
     # Convert the VTK array to vtkImageData
     imdata = vtk.vtkImageData()
-    imdata.SetDimensions(data.shape)
+    imdata.SetDimensions(shape)
     imdata.SetSpacing([res_x,res_y,res_z])
     imdata.SetOrigin([0,0,0])
     imdata.GetPointData().SetScalars(vtk_data_array)
