e0d61c5dfceaf2baa81d11befbb7d2aaf49c794d
Cedric Travelletti <cedrictravelletti@gmail.com>
2020-02-06 14:29:58 +0100

    GaussianProcess Refactor: refactored condition_model.

--- volcapy/inverse/inverse_gaussian_process.py
+++ volcapy/inverse/inverse_gaussian_process.py
@@ -157,9 +157,10 @@ class InverseGaussianProcess(torch.nn.Module):
         self.pushfwd = self.kernel.compute_cov_pushforward(
                 self.lambda0, G, self.cells_coords,
                 self.output_device, n_chunks=200, n_flush=50)
-        self.K_d = torch.mm(G, self.pushfwd)
+        self.K_d = G @ self.pushfwd
 
-    def condition_data(self, G, y, data_std, concentrate=False):
+    def condition_data(self, G, y, data_std, concentrate=False,
+            is_precomp_pushfwd=False):
         """ Given a bunch of measurement, condition model on the data side.
         I.e. only compute the conditional law of the data vector G Z, not of Z
         itself.
@@ -176,6 +177,9 @@ class InverseGaussianProcess(torch.nn.Module):
             If true, then will compute m0 by MLE via concentration of the
             log-likelihood instead of using the current value of the
             hyperparameter.
+        is_precomp_pushfwd: bool
+            Set to True if the covariance pushforward has already been computed
+            by a previous operation.
 
         Returns
         -------
@@ -183,8 +187,8 @@ class InverseGaussianProcess(torch.nn.Module):
             Posterior mean data vector
 
         """
-        self.compute_pushfwd(G)
-        self.K_d = G @ self.pushfwd
+        if not is_precomp_pushfwd:
+            self.compute_pushfwd(G)
 
         # Get Cholesky factor (lower triangular) of the inversion operator.
         self.inv_op_L = self.get_inversion_op_cholesky(self.K_d, self.sigma0)
@@ -193,11 +197,12 @@ class InverseGaussianProcess(torch.nn.Module):
         if concentrate:
             # Determine m0 (on the model side) from sigma0 by concentration of the Ll.
             m0 = self.concentrate_m0()
+        else: m0 = self.m0
 
         # Prior mean (vector) on the data side.
         mu0_d_stripped = torch.mm(G, torch.ones((self.n_model, 1),
                 dtype=torch.float32))
-        mu0_d = self.m0 * mu0_d_stripped
+        mu0_d = m0 * mu0_d_stripped
         prior_misfit = y - mu0_d
 
         self.weights = self.inv_op_vector_mult(prior_misfit)
@@ -206,13 +211,17 @@ class InverseGaussianProcess(torch.nn.Module):
 
         return mu_post_d
 
-    def neg_log_likelihood(self):
+    def neg_log_likelihood(self, y):
         """ Computes the negative log-likelihood of the current state of the
         model.
         Note that this function should be called AFTER having run a
         conditioning, since it depends on the inversion operator computed
         there.
 
+        Params
+        ------
+        y: tensor
+            Data vector
         Returns
         -------
         float
@@ -221,43 +230,48 @@ class InverseGaussianProcess(torch.nn.Module):
         # WARNING!!! determinant is not linear! Taking constants outside adds
         # power to them.
         log_det = torch.logdet(self.R)
-        nll = log_det + torch.mm(self.prior_misfit.t(), self.weights)
+        prior_misfit = y - mu0_d
+        weights = self.inv_op_vector_mult(prior_misfit)
+        nll = log_det + torch.mm(prior_misfit.t(), weights)
 
         return nll
 
-    def concentrate_m0(self):
+    def concentrate_m0(self, y):
         """ Compute m0 (prior mean parameter) by MLE via concentration.
 
         Note that the inversion operator should have been updated first.
 
         """
+        # Prior mean (vector) on the data side.
+        mu0_d_stripped = torch.mm(G, torch.ones((self.n_model, 1),
+                dtype=torch.float32))
         # Compute R^(-1) * G * I_m.
-        tmp = self.inv_op_vector_mult(self.mu0_d_stripped)
-        conc_m0 = torch.mm(self.d_obs.t(), tmp) / torch.mm(self.mu0_d_stripped.t(), tmp)
+        tmp = self.inv_op_vector_mult(mu0_d_stripped)
+        conc_m0 = (y.t() @ tmp) / (mu0_d_stripped.t() @ tmp)
 
         return conc_m0
 
-    def condition_model(self, cov_pushfwd, F, sigma0, m0=0.1,
-            concentrate=False, NtV_crit=-1.0):
-        """ Condition model on the model side.
+    def condition_model(self, G, y, data_std, concentrate=False,
+            is_precomp_pushfwd=False):
+        """ Given a bunch of measurement, condition model on the data side.
+        I.e. only compute the conditional law of the data vector G Z, not of Z
+        itself.
 
         Parameters
         ----------
-        cov_pushfwd: tensor
-            Pushforwarded covariance matrix, i.e. K F^T
-        F
-            Forward operator
-        sigma0: float
-            Standard deviation parameter for the kernel.
-        m0: float
-            Prior mean parameter.
+        G: tensor
+            Measurement matrix
+        y: tensor
+            Observed data
+        data_std: flot
+            Data noise standard deviation.
         concentrate: bool
             If true, then will compute m0 by MLE via concentration of the
-            log-likelihood.
-        NtV_crit: (deprecated)
-            Critical Noise to Variance ratio (epsilon/sigma0). We can fix it to avoid
-            numerical instability in the matrix inversion.
-            If provided, will not allow to go below the critical value.
+            log-likelihood instead of using the current value of the
+            hyperparameter.
+        is_precomp_pushfwd: bool
+            Set to True if the covariance pushforward has already been computed
+            by a previous operation.
 
         Returns
         -------
@@ -269,17 +283,20 @@ class InverseGaussianProcess(torch.nn.Module):
         """
         # Conditioning model is just conditioning on data and then computing
         # posterior mean and (co-)variance on model side.
-        K_d = torch.mm(F, cov_pushfwd)
-        mu_post_d = self.condition_data(K_d, sigma0, m0,
-                concentrate=concentrate)
+        mu_post_d = condition_data(G, y, data_std, concentrate=concentrate,
+                is_precomp_pushfwd=is_precomp_pushfwd)
 
         # Posterior model mean.
         # Can re-use the m0 and weights computed by condition_data.
-        self.mu_post_m = torch.add(
-                m0 * torch.ones((self.n_model, 1)),
-                torch.mm(sigma0**2 * cov_pushfwd, self.weights))
+        if concentrate:
+            m0 = self.concentrate_m0()
+        else: m0 = self.m0
 
-        return self.mu_post_m.detach(), self.mu_post_d
+        mu_post_m = (
+                m0 * torch.ones((self.n_model, 1))
+                + (self.sigma0**2 * self.pushfwd @ self.weights))
+
+        return mu_post_m.detach(), mu_post_d
 
     def optimize(self, K_d, n_epochs, device, sigma0_init=None,
             lr=0.007, NtV_crit=-1.0):
@@ -343,183 +360,6 @@ class InverseGaussianProcess(torch.nn.Module):
         self.to_device(cpu)
 
         return
-
-    def post_cov(self, cov_pushfwd, cells_coords, lambda0, sigma0, i, j, cl):
-        """ Get posterior model covariance between two specified cells.
-
-        Parameters
-        ----------
-        cov_pushfwd: tensor
-            Pushforwarded covariance matrix, i.e. K F^T
-        cells_coords: tensor
-            n_cells * n_dims: cells coordinates
-        lambda0: float
-            Lenght-scale parameter
-        sigma0: float
-            Prior variance.
-        i: int
-            Index of first cell (index in the cells_coords array).
-        j: int
-            Index of second cell.
-        cl: module
-            Covariance module implementing the kernel we are working with.
-            The goal is to make this function kernel independent, though its a
-            bit awkward and should be changed to an object oriented approach.
-
-        Returns
-        -------
-        post_cov: float
-            Posterior covariance between model cells i and j.
-
-        """
-        cov = cl.compute_cov(lambda0, cells_coords, i, j)
-
-        post_cov = sigma0**2 * (cov -
-                torch.mm(
-                    sigma0**2 * cov_pushfwd[i, :].reshape(1, -1),
-                    torch.mm(
-                        self.inversion_operator, cov_pushfwd[j, :].reshape(-1, 1))))
-
-        return post_cov
-
-    def compute_post_cov_diag(self, cov_pushfwd, cells_coords, lambda0, sigma0, cl):
-        """ Compute diagonal of the posterior covariance matrix.
-
-        Parameters
-        ----------
-        cov_pushfwd: tensor
-            Pushforwarded covariance matrix, i.e. K F^T
-        cells_coords: tensor
-            n_cells * n_dims: cells coordinates
-        lambda0: float
-            Lenght-scale parameter
-        sigma0: float
-            Prior variance.
-        cl: module
-            Covariance module implementing the kernel we are working with.
-            The goal is to make this function kernel independent, though its a
-            bit awkward and should be changed to an object oriented approach.
-
-        Returns
-        -------
-        post_cov_diag: tensor
-            Vector containing the posterior variance of model cell i as its
-            i-th element.
-
-        """
-        n_cells = cells_coords.shape[0]
-        post_cov_diag = np.zeros(n_cells)
-
-        for i in range(n_cells):
-            post_cov_diag[i] = self.post_cov(
-                    cov_pushfwd, cells_coords, lambda0, sigma0, i, i, cl)
-
-        return post_cov_diag
-
-    def loo_predict(self, loo_ind):
-        """ Leave one out krigging prediction.
-
-        Take the trained hyperparameters. Remove one point from the
-        training set, krig/condition on the remaining point and predict
-        the left out point.
-
-        WARNING: Should have run the forward pass of the model once before,
-        otherwise some of the operators we need (inversion operator) won't have
-        been computed. Also should re-run the forward pass when updating
-        hyperparameters.
-
-        Parameters
-        ----------
-        loo_ind: int
-            Index (in the training set) of the data point to leave out.
-
-        Returns
-        -------
-        float
-            Prediction at left out data point.
-
-        """
-        # Index of the not removed data points.
-        in_inds = list(range(len(self.d_obs)))
-        in_inds.remove(loo_ind)
-
-        # Note that for the dot product, we should have one-dimensional
-        # vectors, hence the strange indexing with the zero.
-        loo_pred = (self.mu0_d[loo_ind] -
-                1/self.inversion_operator[loo_ind, loo_ind].detach() *
-                torch.dot(
-                    self.inversion_operator[loo_ind, in_inds].detach(),
-                    self.prior_misfit[in_inds, 0].detach()))
-
-        return loo_pred.detach()
-
-    def loo_error(self):
-        """ Leave one out cross validation RMSE.
-
-        Take the trained hyperparameters. Remove one point from the
-        training set, krig/condition on the remaining point and predict
-        the left out point.
-        Compute the squared error, repeat for all data points (leaving one out
-        at a time) and average.
-
-        WARNING: Should have run the forward pass of the model once before,
-        otherwise some of the operators we need (inversion operator) won't have
-        been computed. Also should re-run the forward pass when updating
-        hyperparameters.
-
-        Returns
-        -------
-        float
-            RMSE cross-validation error.
-
-        """
-        tot_error = 0
-        for loo_ind in range(len(self.d_obs)):
-            loo_prediction = self.loo_predict(loo_ind)
-            tot_error += (self.d_obs[loo_ind].item() - loo_prediction)**2
-
-        return torch.sqrt((tot_error / len(self.d_obs)))
-
-    def train_RMSE(self):
-        """ Compute current error on training set.
-
-        Returns
-        -------
-        float
-            RMSE on training set.
-
-        """
-        return torch.sqrt(torch.mean(
-            (self.d_obs - self.mu_post_d)**2))
-
-    def condition_number(self, cov_pushfwd, F, sigma0):
-        """ Compute condition number of the inversion matrix R.
-
-        Used to detect numerical instability.
-
-        Parameters
-        ----------
-        cov_pushfwd: tensor
-            Pushforwarded covariance matrix, i.e. K F^T
-        F: tensor
-            Forward operator
-        sigma0: float
-            Standard deviation parameter for the kernel.
-
-        Returns
-        -------
-        float
-            Condition number of the inversion matrix R.
-
-        """
-        # Noise to Variance ratio.
-        NtV = (self.data_std / sigma0)**2
-
-        inv_inversion_operator = torch.add(
-                        NtV * self.data_ones,
-                        torch.mm(F, cov_pushfwd))
-
-        return np.linalg.cond(inv_inversion_operator.detach().numpy())
 
     def get_inversion_op_cholesky(self, K_d, data_std):
         """ Compute the Cholesky decomposition of the inverse of the inversion operator.
